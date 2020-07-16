838da1268296135f515be247e3840894097a608f
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-07-16 00:32:28 -0400

    remove console.log

--- shop-app/src/components/item-detail/item-detal.component.jsx
+++ shop-app/src/components/item-detail/item-detal.component.jsx
@@ -5,7 +5,7 @@ import MenuItem from '../menu-item/menu-item.component';
 import './item-detail.style.css';
 
 function ItemDetail(props) {
-    console.log(props.match.params.di)
+    
     return <div>Detail screen</div>
 }
 
